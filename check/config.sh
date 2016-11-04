609c746d5a5842335b719b6cce288ab21c98c6ad
Yunxing Dai <yunxing@fb.com>
2016-11-03 23:30:49 -0700

    init commit

---
+++ check/config.sh
@@ -0,0 +1,23 @@
+#
+# Settings from the configure script
+#
+
+abs_srcdir=/Users/yunxing/programming/pkg-config/check
+top_srcdir=..
+abs_top_srcdir=/Users/yunxing/programming/pkg-config
+abs_builddir=/Users/yunxing/programming/pkg-config/check
+top_builddir=..
+abs_top_builddir=/Users/yunxing/programming/pkg-config
+prefix=/Users/yunxing/programming/pkg-config/_build
+exec_prefix=${prefix}
+includedir=${prefix}/include
+libdir=${exec_prefix}/lib
+datarootdir=${prefix}/share
+datadir=${datarootdir}
+pc_path="${libdir}/pkgconfig:${datadir}/pkgconfig"
+system_include_path="/usr/include"
+system_library_path="/usr/lib:/lib"
+list_indirect_deps=no
+PACKAGE_VERSION=0.29.1
+native_win32=no
+WINE=
